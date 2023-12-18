b7c0ae7b797b83de9d839455238e423a806b7694
Cismaticowebdev <cismaticowebdev@gmail.com>
2023-12-18 17:25:23 +0000

    Create function to show button of upgrades when certain amount of coins is reached

--- modules/main.js
+++ modules/main.js
@@ -13,10 +13,10 @@ const upgrade5Btn = document.getElementById('upgrade5-btn');
 const upgradeBtnArray = new Array;
 upgradeBtnArray.push(upgrade1Btn, upgrade2Btn, upgrade3Btn, upgrade4Btn, upgrade5Btn);
 const upgrade1 = new Upgrade('Upgrade 1', 10);
-const upgrade2 = new Upgrade('Upgrade 2', 10);
-const upgrade3 = new Upgrade('Upgrade 3', 10);
-const upgrade4 = new Upgrade('Upgrade 4', 10);
-const upgrade5 = new Upgrade('Upgrade 5', 10);
+const upgrade2 = new Upgrade('Upgrade 2', 100);
+const upgrade3 = new Upgrade('Upgrade 3', 2000);
+const upgrade4 = new Upgrade('Upgrade 4', 30000);
+const upgrade5 = new Upgrade('Upgrade 5', 500000);
 const upgradeArray = new Array;
 upgradeArray.push(upgrade1, upgrade2, upgrade3, upgrade4, upgrade5);
 
@@ -40,9 +40,29 @@ function updateScreen() {
     upgrade1SpanCost.textContent = `${upgrade1.cost}`;
 }
 
+
 function gameLoop() {
     Coins.quantity += Coins.quantityPerSecond;
+    checkAvailableUpgrades();
     updateScreen();
 }
 
 setInterval(gameLoop, 1000);
+
+function checkAvailableUpgrades() {
+    if (Coins.quantity >= 100) {
+        upgrade2Btn.classList.remove('hidden');
+    }
+
+    if (Coins.quantity >= 2000) {
+        upgrade3Btn.classList.remove('hidden');
+    }
+
+    if (Coins.quantity >= 30000) {
+        upgrade4Btn.classList.remove('hidden');
+    }
+
+    if (Coins.quantity >= 500000) {
+        upgrade5Btn.classList.remove('hidden');
+    }
+}
